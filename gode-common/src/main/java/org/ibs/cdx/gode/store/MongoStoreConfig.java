bd1f8005ae547e62b78f16e4b4bc26ceddccd36a
Neethu Santhosh <v-nsanthosh@expedia.com>
2020-06-05 17:26:20 +0530

    changing mongo dependency to spring-boot-starter-data-mongodb

--- gode-common/src/main/java/org/ibs/cdx/gode/store/MongoStoreConfig.java
+++ gode-common/src/main/java/org/ibs/cdx/gode/store/MongoStoreConfig.java
@@ -1,11 +1,36 @@
 package org.ibs.cdx.gode.store;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "org.ibs.cdx.gode.entity.*")
 @EntityScan("org.ibs.cdx.gode.entity.*")
-public class MongoStoreConfig {
+@ComponentScan("org.ibs.cdx.gode")
+@PropertySource(value="classpath:gode.properties")
+public class MongoStoreConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    public MongoClient mongoClient() {
+        MongoClient mongoClient = MongoClients.create();
+//        MongoClient mongoClient = MongoClients.create(
+//                MongoClientSettings.builder()
+//                        .applyToClusterSettings(builder ->
+//                                builder.hosts(Arrays.asList(new ServerAddress("hostOne", 27018))))
+//                        .build());
+        return mongoClient;
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "myNewDatabase";
+    }
 }
